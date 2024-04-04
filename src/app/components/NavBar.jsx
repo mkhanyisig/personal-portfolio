94e165262c2aa18126dfdcd0b1d0abda77aff665
Mkhanyisi Gamedze <mkhanyisi.g@gmail.com>
2024-04-04 14:46:32 -0400

    cleanup reactive component styling

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -22,6 +22,7 @@ const NavLinks = [
 
 const NavBar = ({ isBackgroundDark, onBackgroundToggle }) => {
   const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
     <nav
       className={`fixed top-0 left-0 right-0 z-10 ${
@@ -35,48 +36,51 @@ const NavBar = ({ isBackgroundDark, onBackgroundToggle }) => {
         >
           LOGO
         </Link>
-        <div className="mobile-menu block md:hidden">
-          {!navbarOpen ? (
-            <button
-              id="nav-toggle"
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-              onClick={onBackgroundToggle}
-            >
-              <Bars3Icon className="h-5 w-5"></Bars3Icon>
-            </button>
-          ) : (
-            <button
-              id="nav-toggle"
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-              onClick={onBackgroundToggle}
-            >
-              <XMarkIcon className="h-5 w-5"></XMarkIcon>
-            </button>
-          )}
-        </div>
 
-        <div className="menu hidden md:block md:w-auto " id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8">
-            {NavLinks.map((link, index) => (
-              <li key={index}>
-                <NavLink href={link.path} title={link.title} />
-              </li>
-            ))}
-            <li>
+        <div className="flex items-center ml-auto">
+          <button
+            className="flex items-center px-3 py-1  text-slate-200 hover:text-white hover:border-white"
+            onClick={onBackgroundToggle}
+          >
+            {isBackgroundDark ? (
+              <SunIcon className="h-5 w-5" />
+            ) : (
+              <MoonIcon className="h-5 w-5" />
+            )}
+          </button>
+
+          <div className="hidden md:block ml-4">
+            <ul className="flex space-x-8">
+              {NavLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink href={link.path} title={link.title} />
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="mobile-menu block md:hidden ml-4">
+            {!navbarOpen ? (
               <button
+                id="nav-toggle"
                 className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-                onClick={onBackgroundToggle}
+                onClick={() => setNavbarOpen(!navbarOpen)}
               >
-                {isBackgroundDark ? (
-                  <SunIcon className="h-5 w-5" />
-                ) : (
-                  <MoonIcon className="h-5 w-5" />
-                )}
+                <Bars3Icon className="h-5 w-5"></Bars3Icon>
               </button>
-            </li>
-          </ul>
+            ) : (
+              <button
+                id="nav-toggle"
+                className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+                onClick={() => setNavbarOpen(!navbarOpen)}
+              >
+                <XMarkIcon className="h-5 w-5"></XMarkIcon>
+              </button>
+            )}
+          </div>
         </div>
       </div>
+
       {navbarOpen ? <MenuOverlay links={NavLinks} /> : null}
     </nav>
   );
