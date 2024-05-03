4aad226fb956682f50ed2174ea8dfe75428a70c0
Mkhanyisi Gamedze <mkhanyisi.g@gmail.com>
2024-05-03 01:19:48 -0400

    change padding

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -69,7 +69,7 @@ const NavBar = ({ isBackgroundDark, onBackgroundToggle }) => {
             {!navbarOpen ? (
               <button
                 id="nav-toggle"
-                className={`flex items-center px-3 py-2 border rounded ${navOptionsColor}`}
+                className={`flex items-center px-3 py-1 border rounded ${navOptionsColor}`}
                 onClick={() => setNavbarOpen(!navbarOpen)}
               >
                 <Bars3Icon className="h-5 w-5"></Bars3Icon>
@@ -77,7 +77,7 @@ const NavBar = ({ isBackgroundDark, onBackgroundToggle }) => {
             ) : (
               <button
                 id="nav-toggle"
-                className={`flex items-center px-3 py-2 border rounded ${navOptionsColor}`}
+                className={`flex items-center px-3 py-1 border rounded ${navOptionsColor}`}
                 onClick={() => setNavbarOpen(!navbarOpen)}
               >
                 <XMarkIcon className="h-5 w-5"></XMarkIcon>
